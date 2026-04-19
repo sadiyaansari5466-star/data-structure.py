5495c168c23926e6f52f5668a37e43b25213006d
ansari <sadiyaanssari5466@gmail.com>
2026-04-19 12:15:08 +0530

    Add files via upload

---
+++ data sturucture project.py
@@ -0,0 +1,88 @@
+# Stack 
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.stack.pop()
+        return "Stack is empty"
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+    def display(self):
+        print("Stack:", self.stack)
+
+
+# Queue 
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.queue.pop(0)
+        return "Queue is empty"
+
+    def is_empty(self):
+        return len(self.queue) == 0
+
+    def display(self):
+        print("Queue:", self.queue)
+
+
+# Linked List 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        temp.next = new_node
+
+    def display(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+        print("None")
+
+
+# Run 
+s = Stack()
+s.push(10)
+s.push(20)
+s.pop()
+s.display()
+
+q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+q.dequeue()
+q.display()
+
+ll = LinkedList()
+ll.insert(5)
+ll.insert(10)
+ll.insert(15)
+ll.display()
